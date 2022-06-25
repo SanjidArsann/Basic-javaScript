9e99992926eba0a936d8dec7dbd62f6e972ae95f
SanjidArsann <moshiurrahmanm3@gmail.com>
2022-06-24 21:00:11 -0700

    solving

--- more-simple-problem/divisible-by.js
+++ more-simple-problem/divisible-by.js
@@ -1,4 +1,4 @@
-function divisible(num){
+/* function divisible(num){
     if(num % 3 == 0 && num % 5 == 0){
         console.log('foobar');
     }
@@ -15,4 +15,20 @@ function divisible(num){
 }
 
 const myNumber = divisible(16);
-console.log(myNumber);
+console.log(myNumber); */
+
+for(let i = 1; i <= 50; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log('foobar');
+    }
+    else if(i % 3 == 0){
+        console.log('foo');
+    }
+    else if(i % 5 == 0){
+        console.log('bar');
+    }
+    
+    else{
+        console.log(i);
+    }
+}
